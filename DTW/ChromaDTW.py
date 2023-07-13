8dff8cd7ae895eacd6cd92350577800ade16f3f4
Omega20736 <138450811+Omega20736@users.noreply.github.com>
2023-07-13 14:55:03 +0200

    New DTW Algorithm aswell as some music pieces for testing

---
+++ DTW/ChromaDTW.py
@@ -0,0 +1,57 @@
+import numpy as np
+import librosa
+from scipy.spatial.distance import euclidean
+from fastdtw import fastdtw
+
+
+class DTWChromaAligner:
+    def __init__(self, sr=22050, hop_length=512):
+        self.sr = sr
+        self.hop_length = hop_length
+        self.pitch_classes = 'C C# D D# E F F# G G# A A# B'.split()
+
+    def compute_chroma(self, audio_path):
+        y, sr = librosa.load(audio_path, sr=self.sr)
+        chroma = librosa.feature.chroma_stft(y=y, sr=sr, hop_length=self.hop_length)
+        return chroma
+
+    def align(self, audio_path1, audio_path2):
+        self.chroma1 = self.compute_chroma(audio_path1)
+        self.chroma2 = self.compute_chroma(audio_path2)
+
+        distance, path = fastdtw(self.chroma1.T, self.chroma2.T, dist=euclidean)
+
+        return distance, path
+
+    def get_time_differences_and_pitches(self, path):
+        time_differences = []
+        pitch_pairs = []
+        for pair in path:
+            time_diff = (pair[1] - pair[0]) * self.hop_length / self.sr
+            time_differences.append(time_diff)
+
+            pitch1 = self.pitch_classes[np.argmax(self.chroma1[:, pair[0]])]
+            pitch2 = self.pitch_classes[np.argmax(self.chroma2[:, pair[1]])]
+
+            if pitch1 == pitch2:
+                pitch_pairs.append((pitch1, time_diff))
+
+        return pitch_pairs
+
+
+# Instantiate the DTWChromaAligner class
+aligner = DTWChromaAligner()
+
+# Compute alignment
+# Change directories here
+# distance, path = aligner.align('C:/Users/Omega/Music/MusicAI/(Spotify).mp3', 'C:/Users/Omega/Music/MusicAI/Concert.mp3') # Chopin
+distance, path = aligner.align('C:/Users/Omega/Music/MusicAI/langSiciliano.mp3', 'C:/Users/Omega/Music/MusicAI/SicilianoBWV1031.mp3') # Siciliano
+
+# Compute time differences and pitches
+pitch_pairs = aligner.get_time_differences_and_pitches(path)
+
+# Print the time differences in seconds and the pitches
+for i, (pitch, time_diff) in enumerate(pitch_pairs):
+    print(f"Step {i}: Pitch: {pitch}, Time difference: {time_diff} seconds")
+
+
---
+++ DTW/DTW.py
@@ -0,0 +1,49 @@
+import numpy as np
+import librosa
+from dtw import *
+
+def detect_onsets(audio_file, start_time=0.0):
+    # Load the audio file
+    y, sr = librosa.load(audio_file)
+
+    # Calculate the sample to start from
+    start_sample = int(start_time * sr)
+
+    # Trim the start of the audio file
+    y = y[start_sample:]
+
+    # Detect onsets
+    onsets = librosa.onset.onset_detect(y=y, sr=sr, units='time')
+
+    return onsets
+
+#def detect_onsets(audio_file):
+#    y, sr = librosa.load(audio_file)
+#    onsets = librosa.onset.onset_detect(y=y, sr=sr, units='time')
+#    print(f"Onsets shape for {audio_file}: {onsets.shape}")  # This should print: (n,) for some integer n
+#    return onsets
+
+# Load onsets
+# change directories here
+# Chopin
+#reference_onsets = detect_onsets('C:/Users/Omega/Music/MusicAI/Concert.mp3', start_time=1.0)
+#played_onsets = detect_onsets('C:/Users/Omega/Music/MusicAI/(Spotify).mp3', start_time=7.8)
+
+# Siciliano
+reference_onsets = detect_onsets('C:/Users/Omega/Music/MusicAI/langSiciliano.mp3', start_time=3.0)
+played_onsets = detect_onsets('C:/Users/Omega/Music/MusicAI/SicilianoBWV1031.mp3', start_time=3.5)
+
+# Create the sequences
+s1 = np.array(reference_onsets, ndmin=2).T
+s2 = np.array(played_onsets, ndmin=2).T
+
+# Compute DTW
+alignment = dtw(s1, s2)
+
+# Compute timing differences
+for ref_index, play_index in zip(alignment.index1, alignment.index2):
+    timing_difference = played_onsets[play_index] - reference_onsets[ref_index]
+    print(f'Note {ref_index}:')
+    print(f'\tReference onset time: {reference_onsets[ref_index]} seconds')
+    print(f'\tPlayed onset time: {played_onsets[play_index]} seconds')
+    print(f'\tTiming difference: {timing_difference} seconds')
